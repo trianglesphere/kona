00347b5580c03c349ec823ae613ec7ef7421bd5f
clabby <ben@clab.by>
2025-05-02 13:52:01 -0400

    chore(interop): Reduce message expiry window (#1636)

--- crates/protocol/interop/src/constants.rs
+++ crates/protocol/interop/src/constants.rs
@@ -2,7 +2,7 @@
 
 /// The expiry window for relaying an initiating message (in seconds).
 /// <https://specs.optimism.io/interop/messaging.html#message-expiry-invariant>
-pub const MESSAGE_EXPIRY_WINDOW: u64 = 30 * 24 * 60 * 60;
+pub const MESSAGE_EXPIRY_WINDOW: u64 = 7 * 24 * 60 * 60;
 
 /// The current version of the [SuperRoot] encoding format.
 ///
