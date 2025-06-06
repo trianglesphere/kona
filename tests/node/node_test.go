ccb322b6b10916e5f86438a9b3419f754e57c3d3
theo <80177219+theochap@users.noreply.github.com>
2025-06-06 12:30:24 -0400

    fix(node/p2p): reactivate e2e tests for gossip. (#2036)
    ## Description
    
    This PR reactivates e2e tests for p2p gossip. I had to change the link
    to the stable branch of the `optimism-package` because it appears that
    the main version is broken

--- tests/node/node_test.go
+++ tests/node/node_test.go
@@ -11,7 +11,6 @@ import (
 // This assumes there is at least two L2 chains. The second chain is used to test a larger network.
 func TestSystemNodeP2p(t *testing.T) {
 	t.Parallel()
-	t.Skip("TODO(@theochap): for now this test is disabled because it is very flaky. Once we have better p2p performance, we should re-enable this test.")
 
 	systest.SystemTest(t,
 		allPeersInNetwork(),
@@ -28,11 +27,10 @@ func TestSystemNodeP2p(t *testing.T) {
 // TODO(@theochap): for now this test is disabled because it is very flaky.
 // Once we have better p2p performance, we should re-enable this test.
 func TestSystemNodeP2pLargeNetwork(t *testing.T) {
-	t.Skip("TODO(@theochap): for now this test is disabled because it is very flaky. Once we have better p2p performance, we should re-enable this test.")
 	// Check that the node has at least 4 peers that are connected to its topics when there is more than 9 peers in the network initially.
 	// We put a lower bound on the number of connected peers to account for network instability.
 	systest.SystemTest(t,
-		peerCount(5, 3),
+		peerCount(5, 5),
 		validators.HasSufficientL2Nodes(0, 6),
 	)
 }
