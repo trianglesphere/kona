1b093d95fb9c427b39c107e39dbba3af6413f724
clabby <ben@clab.by>
2025-07-09 09:58:57 -0400

    feat(node/service): Add L1 origin consistency check (#2373)
    ## Overview
    
    Adds the consistency check for the L1 origin during the sequencer
    actor's block building routine.
    
    closes #2370

--- crates/node/service/src/actors/sequencer/actor.rs
+++ crates/node/service/src/actors/sequencer/actor.rs
@@ -163,7 +163,23 @@ impl<AB: AttributesBuilder> SequencerActorState<AB> {
         let unsafe_head = *unsafe_head_rx.borrow();
         let l1_origin = self.origin_selector.next_l1_origin(unsafe_head).await?;
 
-        // TODO(clabby): Check for consistent L1 origin
+        if unsafe_head.l1_origin.hash != l1_origin.parent_hash &&
+            unsafe_head.l1_origin.hash != l1_origin.hash
+        {
+            warn!(
+                target: "sequencer",
+                l1_origin = ?l1_origin,
+                unsafe_head_hash = %unsafe_head.l1_origin.hash,
+                unsafe_head_l1_origin = ?unsafe_head.l1_origin,
+                "Cannot build new L2 block on inconsistent L1 origin, resetting engine"
+            );
+            if let Err(err) = ctx.reset_request_tx.send(()).await {
+                error!(target: "sequencer", ?err, "Failed to reset engine");
+                ctx.cancellation.cancel();
+                return Err(SequencerActorError::ChannelClosed);
+            }
+            return Ok(());
+        }
 
         info!(
             target: "sequencer",
