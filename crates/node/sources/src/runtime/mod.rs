2d8ba0802e801a70d2006e35e6bc4d7c3065011f
longhutianjie <keplrnewton@icloud.com>
2025-08-11 12:52:00 +0800

    chore: fix 404 URL (#2671)
    fix 404 URL
    
    Signed-off-by: longhutianjie <keplrnewton@icloud.com>

--- crates/node/sources/src/runtime/mod.rs
+++ crates/node/sources/src/runtime/mod.rs
@@ -1,7 +1,7 @@
 //! Runtime Loading
 //!
 //! Adapted from the `op-node`
-//! <https://github.com/ethereum-optimism/optimism/blob/develop/op-node/node/runtime_config.go>.
+//! <https://github.com/ethereum-optimism/optimism/blob/develop/op-node/node/runcfg/runtime_config.go>.
 
 mod config;
 pub use config::RuntimeConfig;
