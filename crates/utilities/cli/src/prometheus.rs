c707e4ad1bb515aa49ae9efb0d83e81d07be595c
jagroot <4516586+itschaindev@users.noreply.github.com>
2025-08-21 13:29:04 +0200

    chore (supervisor): add global metrics (#2738)
    Closes #2714 and #2713 
    
    Dashboard preview:
    
    <img width="1845" height="281" alt="Screenshot 2025-08-20 at 15 43 45"
    src="https://github.com/user-attachments/assets/101c7cb2-f900-40fa-b708-6277930d11e2"
    />
    
    This should also work with other Kona services.

--- crates/utilities/cli/src/prometheus.rs
+++ crates/utilities/cli/src/prometheus.rs
@@ -1,7 +1,12 @@
 //! Utilities for spinning up a prometheus metrics server.
 
 use metrics_exporter_prometheus::{BuildError, PrometheusBuilder};
-use std::net::{IpAddr, SocketAddr};
+use metrics_process::Collector;
+use std::{
+    net::{IpAddr, SocketAddr},
+    thread::{self, sleep},
+    time::Duration,
+};
 use tracing::info;
 
 /// Start a Prometheus metrics server on the given port.
@@ -10,6 +15,18 @@ pub fn init_prometheus_server(addr: IpAddr, metrics_port: u16) -> Result<(), Bui
     let builder = PrometheusBuilder::new().with_http_listener(prometheus_addr);
 
     builder.install()?;
+
+    // Initialise collector for system metrics e.g. CPU, memory, etc.
+    let collector = Collector::default();
+    collector.describe();
+
+    thread::spawn(move || {
+        loop {
+            collector.collect();
+            sleep(Duration::from_secs(60));
+        }
+    });
+
     info!(
         target: "prometheus",
         "Serving metrics at: http://{}",
