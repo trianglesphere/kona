dc7ee954de43d4158318d9e3aeafc60a3adb905e
theochap <theodore.chap@gmail.com>
2025-08-22 21:11:35 +0200

    feat(node/test): rewrite connection drop and sync tests

--- tests/node/restart/conn_drop_test.go
+++ tests/node/restart/conn_drop_test.go
@@ -1,12 +1,13 @@
 package node_restart
 
 import (
-	"sync"
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/ethereum-optimism/optimism/op-devstack/devtest"
 	"github.com/ethereum-optimism/optimism/op-devstack/dsl"
+	"github.com/ethereum-optimism/optimism/op-service/retry"
 	"github.com/ethereum-optimism/optimism/op-supervisor/supervisor/types"
 	node_utils "github.com/op-rs/kona/node/utils"
 )
@@ -17,52 +18,82 @@ func TestConnDropSync(gt *testing.T) {
 
 	out := node_utils.NewMixedOpKona(t)
 
-	nodes := out.L2CLKonaValidatorNodes
+	nodes := out.L2CLValidatorNodes()
 	sequencerNodes := out.L2CLSequencerNodes()
 	t.Gate().Greater(len(nodes), 0, "expected at least one validator node")
 	t.Gate().Greater(len(sequencerNodes), 0, "expected at least one sequencer node")
 
 	sequencer := sequencerNodes[0]
 
-	var wg sync.WaitGroup
+	var postDisconnectCheckFuns []dsl.CheckFunc
 	for _, node := range nodes {
-		wg.Add(1)
-		go func(node *dsl.L2CLNode) {
-			defer wg.Done()
-			clName := node.Escape().ID().Key()
+		clName := node.Escape().ID().Key()
 
-			node.DisconnectPeer(&sequencer)
+		node.DisconnectPeer(&sequencer)
 
-			// Wait for 2 minutes
-			time.Sleep(2 * time.Minute)
+		// Ensure that the node is no longer connected to the sequencer
+		seqPeers := sequencer.Peers()
+		for _, peer := range seqPeers.Peers {
+			t.Require().NotEqual(peer.PeerID, node.PeerInfo().PeerID, "expected node %s to be disconnected from sequencer %s", clName, sequencer.Escape().ID().Key())
+		}
 
-			// Ensure that the node is no longer connected to the sequencer
-			seqPeers := sequencer.Peers()
-			for _, peer := range seqPeers.Peers {
-				t.Require().NotEqual(peer.PeerID, node.PeerInfo().PeerID, "expected node %s to be disconnected from sequencer %s", clName, sequencer.Escape().ID().Key())
-			}
-
-			node.ConnectPeer(&sequencer)
-
-			// Check that the node is resyncing with the network
-			dsl.CheckAll(t, node.MatchedFn(&sequencer, types.LocalSafe, 50), node.MatchedFn(&sequencer, types.LocalUnsafe, 50))
-
-			// Check that the node is connected to the reference node
-			peers := node.Peers()
-			t.Require().Greater(len(peers.Peers), 0, "expected at least one peer")
-
-			// Check that there is at least a peer with the same ID as the ref node
-			found := false
-			for _, peer := range peers.Peers {
-				if peer.PeerID == sequencer.PeerInfo().PeerID {
-					t.Logf("node %s is connected to reference node %s", clName, sequencer.Escape().ID().Key())
-					found = true
-					break
-				}
-			}
-
-			t.Require().True(found, "expected node %s to be connected to reference node %s", clName, sequencer.Escape().ID().Key())
-		}(&node)
+		// Check that...
+		// - the node's safe head is advancing
+		// - the node's unsafe head is advancing (through consolidation)
+		// - the node's safe head's number is catching up with the unsafe head's number
+		// - the node's unsafe head is strictly lagging behind the sequencer's unsafe head
+		postDisconnectCheckFuns = append(postDisconnectCheckFuns, node.AdvancedFn(types.LocalSafe, 50, 200), node.AdvancedFn(types.LocalUnsafe, 50, 200), SafeUnsafeMatchedFn(t, node, 50))
 	}
-	wg.Wait()
+
+	postDisconnectCheckFuns = append(postDisconnectCheckFuns, sequencer.AdvancedFn(types.LocalUnsafe, 50, 200))
+
+	dsl.CheckAll(t, postDisconnectCheckFuns...)
+
+	var postReconnectCheckFuns []dsl.CheckFunc
+	for _, node := range nodes {
+		clName := node.Escape().ID().Key()
+
+		node.ConnectPeer(&sequencer)
+
+		// Check that the node is connected to the reference node
+		peers := node.Peers()
+		t.Require().Greater(len(peers.Peers), 0, "expected at least one peer")
+
+		// Check that there is at least a peer with the same ID as the ref node
+		found := false
+		for _, peer := range peers.Peers {
+			if peer.PeerID == sequencer.PeerInfo().PeerID {
+				t.Logf("node %s is connected to reference node %s", clName, sequencer.Escape().ID().Key())
+				found = true
+				break
+			}
+		}
+
+		t.Require().True(found, "expected node %s to be connected to reference node %s", clName, sequencer.Escape().ID().Key())
+
+		// Check that the node is resyncing with the unsafe head network
+		postReconnectCheckFuns = append(postReconnectCheckFuns, node.MatchedFn(&sequencer, types.LocalSafe, 50), node.MatchedFn(&sequencer, types.LocalUnsafe, 50))
+	}
+
+	dsl.CheckAll(t, postReconnectCheckFuns...)
+}
+
+// MatchedFn returns a lambda that checks the baseNode head with given safety level is matched with the refNode chain sync status provider
+// Composable with other lambdas to wait in parallel
+func SafeUnsafeMatchedFn(t devtest.T, clNode dsl.L2CLNode, attempts int) dsl.CheckFunc {
+	logger := t.Logger()
+	chainID := clNode.ChainID()
+	return func() error {
+		return retry.Do0(t.Ctx(), attempts, &retry.FixedStrategy{Dur: 2 * time.Second},
+			func() error {
+				base := clNode.ChainSyncStatus(chainID, types.LocalSafe)
+				ref := clNode.ChainSyncStatus(chainID, types.LocalUnsafe)
+				if ref.Hash == base.Hash && ref.Number == base.Number {
+					logger.Info("Node safe and unsafe heads matched", "ref", ref.Number, "base", base.Number)
+					return nil
+				}
+				logger.Info("Node safe and unsafe heads not matched", "safe", base.Number, "unsafe", ref.Number, "ref", ref.Hash, "base", base.Hash)
+				return fmt.Errorf("expected safe and unsafe heads to match")
+			})
+	}
 }
--- tests/node/restart/restart_test.go
+++ tests/node/restart/restart_test.go
@@ -2,9 +2,7 @@ package node_restart
 
 import (
 	"context"
-	"sync"
 	"testing"
-	"time"
 
 	"github.com/ethereum-optimism/optimism/op-devstack/devtest"
 	"github.com/ethereum-optimism/optimism/op-devstack/dsl"
@@ -26,56 +24,61 @@ func TestRestartSync(gt *testing.T) {
 
 	sequencer := sequencerNodes[0]
 
-	var wg sync.WaitGroup
+	var preStopCheckFuns []dsl.CheckFunc
 	for _, node := range nodes {
 		t.Logf("testing restarts for node %s", node.Escape().ID().Key())
-		wg.Add(1)
-		go func(node *dsl.L2CLNode) {
-			defer wg.Done()
-			clName := node.Escape().ID().Key()
+		clName := node.Escape().ID().Key()
+		nodePeerId := node.PeerInfo().PeerID
 
-			t.Logf("stopping node %s", clName)
-			node.Stop()
+		t.Logf("stopping node %s", clName)
+		node.Stop()
 
-			// Ensure that the node is no longer connected to the sequencer
-			seqPeers := sequencer.Peers()
-			for _, peer := range seqPeers.Peers {
-				t.Require().NotEqual(peer.PeerID, node.PeerInfo().PeerID, "expected node %s to be disconnected from sequencer %s", clName, sequencer.Escape().ID().Key())
-			}
+		// Ensure that the node is no longer connected to the sequencer
+		seqPeers := sequencer.Peers()
+		for _, peer := range seqPeers.Peers {
+			t.Require().NotEqual(peer.PeerID, nodePeerId, "expected node %s to be disconnected from sequencer %s", clName, sequencer.Escape().ID().Key())
+		}
 
-			// Ensure that the node is stopped
-			// Check that calling any rpc method returns an error
-			rpc := node_utils.GetNodeRPCEndpoint(node)
-			var out *eth.SyncStatus
-			err := rpc.CallContext(context.Background(), &out, "opp2p_syncStatus")
-			t.Require().Error(err, "expected node %s to be stopped", clName)
+		// Ensure that the node is stopped
+		// Check that calling any rpc method returns an error
+		rpc := node_utils.GetNodeRPCEndpoint(&node)
+		var out *eth.SyncStatus
+		err := rpc.CallContext(context.Background(), &out, "opp2p_syncStatus")
+		t.Require().Error(err, "expected node %s to be stopped", clName)
 
-			time.Sleep(2 * time.Minute)
-
-			t.Logf("starting node %s", clName)
-			node.Start()
-
-			node.ConnectPeer(&sequencer)
-
-			// Check that the node is resyncing with the network
-			dsl.CheckAll(t, node.MatchedFn(&sequencer, types.LocalSafe, 50), node.MatchedFn(&sequencer, types.LocalUnsafe, 50))
-
-			// Check that the node is connected to the reference node
-			peers := node.Peers()
-			t.Require().Greater(len(peers.Peers), 0, "expected at least one peer")
-
-			// Check that there is at least a peer with the same ID as the ref node
-			found := false
-			for _, peer := range peers.Peers {
-				if peer.PeerID == sequencer.PeerInfo().PeerID {
-					t.Logf("node %s is connected to reference node %s", clName, sequencer.Escape().ID().Key())
-					found = true
-					break
-				}
-			}
-
-			t.Require().True(found, "expected node %s to be connected to reference node %s", clName, sequencer.Escape().ID().Key())
-		}(&node)
+		// Ensure that the sequencer's head is advancing.
+		preStopCheckFuns = append(preStopCheckFuns, sequencer.AdvancedFn(types.LocalUnsafe, 50, 200))
 	}
-	wg.Wait()
+
+	dsl.CheckAll(t, preStopCheckFuns...)
+
+	var postStartCheckFuns []dsl.CheckFunc
+	for _, node := range nodes {
+		clName := node.Escape().ID().Key()
+		t.Logf("starting node %s", clName)
+		node.Start()
+
+		node.ConnectPeer(&sequencer)
+
+		// Check that the node is resyncing with the network
+		postStartCheckFuns = append(postStartCheckFuns, node.MatchedFn(&sequencer, types.LocalSafe, 50), node.MatchedFn(&sequencer, types.LocalUnsafe, 50))
+
+		// Check that the node is connected to the reference node
+		peers := node.Peers()
+		t.Require().Greater(len(peers.Peers), 0, "expected at least one peer")
+
+		// Check that there is at least a peer with the same ID as the ref node
+		found := false
+		for _, peer := range peers.Peers {
+			if peer.PeerID == sequencer.PeerInfo().PeerID {
+				t.Logf("node %s is connected to reference node %s", clName, sequencer.Escape().ID().Key())
+				found = true
+				break
+			}
+		}
+
+		t.Require().True(found, "expected node %s to be connected to reference node %s", clName, sequencer.Escape().ID().Key())
+	}
+
+	dsl.CheckAll(t, postStartCheckFuns...)
 }
