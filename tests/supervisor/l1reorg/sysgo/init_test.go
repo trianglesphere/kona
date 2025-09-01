c67022ef162de515ff5fb074ef98951bfa7dcc18
sadiq1971 <sadiqurr8@gmail.com>
2025-09-02 01:51:50 +0600

    updated l1reorg sysgo test to accept custom preset

--- tests/supervisor/l1reorg/sysgo/init_test.go
+++ tests/supervisor/l1reorg/sysgo/init_test.go
@@ -4,10 +4,11 @@ import (
 	"testing"
 
 	"github.com/ethereum-optimism/optimism/op-devstack/presets"
+	spresets "github.com/op-rs/kona/supervisor/presets"
 )
 
 // TestMain creates the test-setups against the shared backend
 func TestMain(m *testing.M) {
 	// Other setups may be added here, hydrated from the same orchestrator
-	presets.DoMain(m, presets.WithSimpleInterop())
+	presets.DoMain(m, spresets.WithSimpleInteropMinimal())
 }
